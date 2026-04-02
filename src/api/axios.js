ed9c3ae25809e2d8957080306a3f5b0580efb8ae
Tbrainyz <gbadebo.wale004@gmail.com>
2026-04-02 13:43:01 +0100

    good

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, //coming from .env file
+  baseURL: import.meta.env.VITE_API_URL, // now points to /api
 });
 
 // Attach the token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
