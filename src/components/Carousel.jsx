768126b188d0297d5d7c6f341f5459ee06dee622
Tbrainyz <gbadebo.wale004@gmail.com>
2026-04-06 11:54:00 +0100

    fix

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -37,7 +37,7 @@ const Carousel = () => {
                 <img
                   src={task.image || "/default-image.png"}
                   alt={task.title}
-                  className="w-full h-40 sm:h-44 object-cover object-top"
+                  className="w-full h-40 sm:h-44 object-cover object-center"
                 />
               </div>
 
